e2d670788c87822636ca6b34bc1d584fd72e577c
Eric <info@ericparlier.com>
2023-01-06 14:58:07 -0500

    1.0.0

---
+++ resources/js/views/view.NewBook.js
@@ -0,0 +1,67 @@
+require('Views.NewBook', 'View');
+OGX.Views.NewBook = function(__config){
+    construct(this, 'Views.NewBook');
+	'use strict'; 
+    const books = __config.data;
+    let popup, text;
+    let val = null;
+
+    //@Override
+    this.construct = function(__data){   
+        popup = app.findPopup(this);
+        text = $('#new_book > .text');
+        OGX.Form.bindField({
+            el: '#new_book input',
+            allowed: /[a-zA-Z0-9 ]/,
+            validate:true,
+            wait:0,
+            change_cb:onNameChange
+        });  
+    };
+    
+    //@Override
+	this.enable = function(){};
+	
+    //@Override
+	this.disable = function(){};    
+	
+    //@Override
+	this.ux = function(__bool){
+        if(__bool){
+          
+        }else{
+            
+        }
+    }; 
+    
+    //@Override
+    this.destroy = function(){
+        OGX.Form.unbindField('#new_book input');
+    };
+
+    this.val = function(){
+        return val;
+    };
+
+    function onNameChange(__obj){
+        let t;
+        if(__obj.valid){
+            if(books.get({name:{eq:__obj.value}}, null, 1)){
+                t = 'There is already a book with that name !';
+                val = null;
+                popup.disableButton(0);
+            }else{               
+                t = 'This looks good and original !';
+                val = __obj.value;
+                popup.enableButton(0);                              
+            }    
+            text.html(t);
+        }else{             
+            t = 'Enter book name and hit OK';
+            val = null;           
+            popup.disableButton(0);
+            text.html(t);
+        }
+    }
+
+};
