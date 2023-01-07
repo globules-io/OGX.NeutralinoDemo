740b6348764c2a450f9d4fb78b01bf9406ef4bda
Eric <info@ericparlier.com>
2023-01-07 17:58:46 -0500

    Exit app

--- www/js/bin/main.js
+++ www/js/bin/main.js
@@ -1,5 +1,8 @@
 var app;
 $(document).ready(function(){		
     Neutralino.init();
+    Neutralino.events.on('windowClose', () => {
+        Neutralino.app.exit();
+    });
     app = new OGX.App({encrypted:false});
 });
